3e80414e90de22809f6c9fc0563a872ee67733b6
maftuna <ladybug.paris.05@gmail.com>
2024-09-02 19:34:33 +0500

    Update RewardsConverter.java

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,20 +1,44 @@
 import java.util.Scanner;
 
 public class RewardsConverter {
+
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Credit Card Rewards Converter!");
-        System.out.println("Please enter a cash value to convert to airline miles: ");
-        var input_value = scanner.nextLine();
-        double cashValue;
-        try {
-            cashValue = Double.parseDouble(input_value);
-        } catch (NumberFormatException exception) {
-            System.out.println("Could not parse input value as a double, exiting");
+        RewardsConverter converter = new RewardsConverter();
+        converter.run();
+    }
+
+    public void run() {
+        double cashValue = getInputValue();
+        if (cashValue < 0) {
+            System.out.println("Invalid input. Exiting.");
             return;
         }
-        System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
-        System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+        
+        convertAndDisplayMiles(cashValue);
+    }
+
+    private double getInputValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Credit Card Rewards Converter!");
+        System.out.println("Please enter a cash value to convert to airline miles: ");
+        String inputValue = scanner.nextLine();
+        
+        return parseCashValue(inputValue);
+    }
+
+    private double parseCashValue(String inputValue) {
+        try {
+            return Double.parseDouble(inputValue);
+        } catch (NumberFormatException exception) {
+            System.out.println("Could not parse input value as a double.");
+            return -1;
+        }
+    }
+
+    private void convertAndDisplayMiles(double cashValue) {
+        System.out.println("Converting $" + cashValue + " to miles");
+        RewardValue rewardsValue = new RewardValue(cashValue);
+        System.out.println("$" + cashValue + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
+// I am sorry but I am not quiet good with Java. I know only C++ and Python
